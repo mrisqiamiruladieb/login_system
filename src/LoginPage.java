7420464d43c13fdb9d2c128b36ca519f3e4eb439
adieb <mrizqiamiruladieb@gmail.com>
2025-03-13 14:39:47 +0700

    feat: message labels, user id and password fields

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,3 +1,4 @@
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
@@ -28,8 +29,17 @@ public class LoginPage implements ActionListener {
         userIDLabel.setBounds(50, 100, 75, 25);
         userPasswordLabel.setBounds(50, 150, 75, 25);
         
+        messageLabel.setBounds(125, 250, 250, 35);
+        messageLabel.setFont(new Font(null, Font.ITALIC, 25));
+                
+        userIDField.setBounds(125, 100, 200, 25);
+        userPasswordField.setBounds(125, 150, 200, 25);
+        
         frame.add(userIDLabel);
         frame.add(userPasswordLabel);
+        frame.add(messageLabel);
+        frame.add(userIDField);
+        frame.add(userPasswordField);
         
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420, 420);

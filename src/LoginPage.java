41dbb7777f155b8395c58a401850b5116bc39959
adieb <mrizqiamiruladieb@gmail.com>
2025-03-13 14:15:42 +0700

    feat: creating instance and construct of login page

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,4 +1,19 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 
-public class LoginPage {
+public class LoginPage implements ActionListener {
     
+    HashMap<String, String> loginInfo = new HashMap<String, String>();
+            
+    //construct
+    LoginPage(HashMap<String, String> loginInfoOriginal) {
+        
+        loginInfo = loginInfoOriginal;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }

481aa61b77d8fa70df3724d725b7748b24b346a7
adieb <mrizqiamiruladieb@gmail.com>
2025-03-13 14:07:31 +0700

    feat: creating hash maps, construct, method

--- src/IDandPasswords.java
+++ src/IDandPasswords.java
@@ -1,4 +1,19 @@
+import java.util.HashMap;
 
 public class IDandPasswords {
     
+    HashMap<String, String> loginInfo = new HashMap<String, String>();
+    
+    //construct
+    IDandPasswords() {
+        
+        loginInfo.put("Joe Vega", "viperr");
+        loginInfo.put("Callahan", "mustangt");
+        loginInfo.put("Kaze", "mclerinn");
+    }
+    
+    //method
+    protected HashMap getLoginInfo() {
+        return loginInfo;
+    }
 }

ecf08671882af9cf558365510aae566b1d3b8f0b
adieb <mrizqiamiruladieb@gmail.com>
2025-03-13 14:24:23 +0700

    feat: instantiate fields

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,9 +1,23 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 public class LoginPage implements ActionListener {
     
+    //declare
+    JFrame frame = new JFrame();
+    JButton loginButton = new JButton("Login");
+    JButton resetButton = new JButton("Reset");
+    JTextField userIDField = new JTextField();
+    JPasswordField userPasswordField = new JPasswordField();
+    JLabel userIDLabel = new JLabel("userID:");
+    JLabel userPasswordLabel = new JLabel("password:");
+    JLabel messageLabel = new JLabel();
     HashMap<String, String> loginInfo = new HashMap<String, String>();
             
     //construct
@@ -12,6 +26,7 @@ public class LoginPage implements ActionListener {
         loginInfo = loginInfoOriginal;
     }
 
+    //method
     @Override
     public void actionPerformed(ActionEvent e) {
 
